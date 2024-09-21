a8b4a5b344dab7c714f828dd0ff41b53d6bcff07
clj1314 <138344370+clj1314@users.noreply.github.com>
2024-09-21 17:56:45 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,7 +1,5 @@
 const routes = [
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
-  {name: 'Jy解析', url: 'https://jx.playerjy.com/?ads=0&url='},
-  {name: 'nx解析', url: 'https://jx.nnxv.cn/tv.php?url='},
   {name: 'nx解析', url: 'https://jx.ppflv.com/?url='},
 ]
 
